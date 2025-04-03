e2038fcfa3281e397dc378cd80feab8d558563f0
Guilherme A. R <guiabucarma@hotmail.com>
2025-04-03 16:22:40 -0400

    Update script TouchController
    remove method start

--- Assets/Scripts/Utils/TouchController.cs
+++ Assets/Scripts/Utils/TouchController.cs
@@ -8,11 +8,6 @@ public class TouchController : MonoBehaviour{
 
     public float velocity = 1f;
 
-    // Start is called before the first frame update
-    void Start(){
-        
-    }
-
     // Update is called once per frame
     void Update(){
         
